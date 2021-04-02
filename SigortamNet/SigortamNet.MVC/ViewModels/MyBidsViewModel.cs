9664232b3c4f4ef9a23159ed52fd86fdef6b99c0
fatihcandan52 <fatihcandan52@gmail.com>
2021-04-02 21:01:32 +0300

    mvc proje sayfa tasarımları tamamlandı

---
+++ SigortamNet/SigortamNet.MVC/ViewModels/MyBidsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SigortamNet.MVC.ViewModels
+{
+    public class MyBidsViewModel
+    {
+        [Display(Name = "T.C. Kimlik Numarası", Prompt = "Lütfen T.C. kimlik numaranızı giriniz")]
+        public string IdentificationNumber { get; set; }
+    }
+}
---
+++ SigortamNet/SigortamNet.MVC/ViewModels/VisitorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SigortamNet.MVC.ViewModels
+{
+    public class VisitorViewModel
+    {
+        [Display(Name = "Plaka")]
+        public string LicensePlate { get; set; }
+        [Display(Name = "T.C. Kimlik Numarası")]
+        public string IdentificationNumber { get; set; }
+        [Display(Name = "Ruhsat Seri Kodu")]
+        public string LicenseSerialCode { get; set; }
+        [Display(Name = "Ruhsat Seri No")]
+        public string LicenseSerialNo { get; set; }
+    }
+}
